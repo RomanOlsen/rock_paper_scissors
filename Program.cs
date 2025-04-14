da48c405b2a40eaa0921a31ffd3707308bf57e4a
Roman <romanjolsen@gmail.com>
2025-04-14 14:35:08 -0600

    convert a move to an answer method

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ internal class Program
 {
   private static void Main()
   {
-    Console.WriteLine("Loading up Rock Paper Scissors game! 🤘");
+    Console.WriteLine("Let's play Rock Paper Scissors! 🤘");
     string userMove = ChooseMove();
     string computerMove = PickComputerMove();
     Console.WriteLine($"You chose {userMove}");
@@ -22,18 +22,50 @@ internal class Program
 
     if (userInput != "r" && userInput != "p" && userInput != "s")
     {
+      Console.Clear();
       Console.WriteLine("Please type r, p, or s to choose.");
       return ChooseMove(); // thanks to jeremy for adding the return for me
     }
+    string answer = FilterMove(userInput);
 
 
-    return $"{userInput}";
+    return $"{answer}";
   }
 
   static string PickComputerMove()
   {
     int computerPick = new Random().Next(1, 4);
-    Console.WriteLine($"{computerPick}");
-    return $"{computerPick}";
+    string answer = FilterMove($"{computerPick}");
+    // if (computerPick == 1)
+    // {
+    //   return "rock";
+    // }
+    // if (computerPick == 2)
+    // {
+    //   return "paper";
+    // }
+    // if (computerPick == 3)
+    // {
+    //   return "scissors";
+    // }
+    return $"{answer}";
+
+  }
+
+  static string FilterMove(string option)
+  {
+    if (option == "1" || option == "r")
+    {
+      return "rock";
+    }
+    if (option == "2" || option == "p")
+    {
+      return "paper";
+    }
+    if (option == "3" || option == "s")
+    {
+      return "scissors";
+    }
+    return "an error occured/something broke";
   }
 }
